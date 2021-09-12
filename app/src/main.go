cabbbf065270b9db015d9fc175646b83484ff333
kyoya <m.kyoya777@gmail.com>
2021-09-12 21:05:15 +0900

    docker-compose upでserverで立つ

--- app/src/main.go
+++ app/src/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func main(){
-
-	fmt.Println("aaaa")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, Echo World!!")
+    })
+    e.Logger.Fatal(e.Start(":8080"))
 }
