44c6feabd9fbcf1d7f89cf3cb62b0dbcda4d9f7a
onyanko-pon <engineer.tsukasa@gmail.com>
2021-09-18 12:00:27 +0900

    connect db

--- app/main.go
+++ app/main.go
@@ -12,27 +12,33 @@ func main() {
 	e := echo.New()
 
 	// TODO 環境変数から取りたい
-	sqlHandler, _ := SQLHandler.NewHandler("user:password@tcp(db:3306)test_database")
+	sqlHandler, err := SQLHandler.NewHandler("user:password@tcp(db:3306)/test_database")
+
+	if err != nil {
+		fmt.Printf("connect error: %s\n", err.Error())
+		panic(1)
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!")
 	})
 
 	e.GET("/users/1", func(c echo.Context) error {
-		query := "select id, name from users where id=1"
+		query := "select id, username from users where id=1"
 		rows, err := sqlHandler.QueryContext(c.Request().Context(), query)
 
 		if err != nil {
-			return c.String(500, "db connect error")
+			return c.String(500, "db exec error")
 		}
 
 		var id string
-		var name string
-		err = rows.Scan(&id, &name)
+		var username string
+		rows.Next()
+		err = rows.Scan(&id, &username)
 		if err != nil {
-			return c.String(500, "db scan error")
+			return c.String(500, fmt.Sprintf("db scan error: %s", err.Error()))
 		}
-		return c.String(http.StatusOK, fmt.Sprintf("id: %s, name: %s", id, name))
+		return c.String(http.StatusOK, fmt.Sprintf("id: %s, username: %s", id, username))
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
