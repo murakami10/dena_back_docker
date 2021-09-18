e2694696a8efa3b193e0db3aec0423fa71108892
yamamoto-yuta <automatuX78@gmail.com>
2021-09-18 17:25:03 +0900

    スペルミスを修正

--- db/3_requests.sql
+++ db/3_requests.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS `requests`;
 CREATE TABLE `requests` (
     `id`   INT NOT NULL AUTO_INCREMENT,
     `sender_id` INT NOT NULL, -- users.id
-    `reciever_id` INT NOT NULL, -- users.id
+    `receiver_id` INT NOT NULL, -- users.id
     `message` text not null,
     `created_at` datetime NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`sender_id`) REFERENCES `users`(`id`),
-    FOREIGN key (`reciever_id`) REFERENCES `users`(`id`)
+    FOREIGN key (`receiver_id`) REFERENCES `users`(`id`)
 );
