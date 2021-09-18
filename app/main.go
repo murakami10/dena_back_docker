d69223d4e3516fa26bb2bcaa586f7ee6eb071b36
yamamoto-yuta <automatuX78@gmail.com>
2021-09-18 11:52:06 +0900

    Echoの動作チェック中

--- app/main.go
+++ app/main.go
@@ -6,10 +6,33 @@ import (
 	"github.com/labstack/echo"
 )
 
+type User struct {
+    Name  string `json:"name"`
+    Email string `json:"email"`
+}
+
+func show(c echo.Context) error {
+    u := new(User)
+    if err := c.Bind(u); err != nil {
+        return err
+    }
+    return c.JSON(http.StatusOK, u)
+}
+
+func display(c echo.Context) error {
+    u := new(User)
+    if err := c.Bind(u); err != nil {
+        return err
+    }
+    return c.JSON(http.StatusOK, u)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!")
 	})
+	e.GET("/user", show)
+	e.POST("/users", display)
 	e.Logger.Fatal(e.Start(":8080"))
 }
