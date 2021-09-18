b607dd64147cc83af420f2a2edecafa8c504003c
yamamoto-yuta <automatuX78@gmail.com>
2021-09-18 21:40:46 +0900

    default値をcurrent_timestampに設定

--- db/3_requests.sql
+++ db/3_requests.sql
@@ -5,7 +5,7 @@ CREATE TABLE `requests` (
     `sender_id` INT NOT NULL, -- users.id
     `receiver_id` INT NOT NULL, -- users.id
     `message` text not null,
-    `created_at` datetime NOT NULL,
+    `created_at` datetime not null default current_timestamp,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`sender_id`) REFERENCES `users`(`id`),
     FOREIGN key (`receiver_id`) REFERENCES `users`(`id`)
